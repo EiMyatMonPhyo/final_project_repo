21c728c1f3ab56ab86529d5944c7a125d5829a33
EiMyatMonPhyo <eimyatmonphyo@gmail.com>
2026-02-01 02:34:39 +0630

    API added for baselines

--- Django Project/musicRecommender/nextTrackMR/api.py
+++ Django Project/musicRecommender/nextTrackMR/api.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from .models import *
 from .serializers import *
 
-from .recommenderLogic import recommend_Euclidean
+from .recommenderLogic import recommend_Euclidean, recommend_Cosine, recommend_random_by_artist
 
 @api_view(['POST'])
 def recommendTrackId(request):
@@ -27,3 +27,45 @@ def recommendTrackId(request):
         serializer_output = TrackIdRecommendationSerializer(track)
         return Response(serializer_output.data)
     return Response (serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def recommendTrackIdCosine(request):
+    serializer = RecommenderInputSerializer(data=request.data)
+
+    if serializer.is_valid():
+        input_track_ids = serializer.validated_data.get('track_ids')
+        input_preferences = serializer.validated_data.get('preferences') or {"energy_weight" : 1.0, "tempo_weight" : 1.0}
+
+        
+        try:
+            track = recommend_Cosine(input_track_ids, input_preferences)
+
+        except ValueError as e: 
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # put output to serializer
+        serializer_output = TrackIdRecommendationSerializer(track)
+        return Response(serializer_output.data)
+    return Response (serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def recommendTrackIdRandom(request):
+    serializer = RecommenderInputSerializer(data=request.data)
+
+    if serializer.is_valid():
+        input_track_ids = serializer.validated_data.get('track_ids')
+
+        
+        try:
+            track = recommend_random_by_artist(input_track_ids)
+
+        except ValueError as e: 
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # put output to serializer
+        serializer_output = TrackIdRecommendationSerializer(track)
+        return Response(serializer_output.data)
+    return Response (serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
--- Django Project/musicRecommender/nextTrackMR/urls.py
+++ Django Project/musicRecommender/nextTrackMR/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('recommend/', views.recommend, name='recommend_track'),
     path('update_preference/', views.updatePreference, name='update_preference'),
 
-    path('api/recommend/', api.recommendTrackId, name='recommend_track_api')
+    path('api/recommend/', api.recommendTrackId, name='recommend_track_api'),
+    # baseline
+    path('api/recommend/baseline/cosine/', api.recommendTrackIdCosine, name='recommend_track_cosine_api'),
+    path('api/recommend/baseline/random/', api.recommendTrackIdRandom, name='recommend_track_random_api')
 ]
